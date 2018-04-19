27f8d7374fe3fe67335f840f9dd8f6c3bc5f4851
zhangl <244680806@qq.com>
2018-04-19 11:42:36 +0800

    add socialiteproviders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\Weixin\WeixinExtendSocialite;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -16,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\Event' => [
             'App\Listeners\EventListener',
         ],
+        SocialiteWasCalled::class=>[
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
+        ]
     ];
 
     /**
